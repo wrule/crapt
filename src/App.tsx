0d7103d32de0c65f2678ba8acfba4f8ee4b806fa
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-10 14:11:01 +0800

    antd应用

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.scss';
+import { Home } from './views/home';
 
 function App() {
   return (
     <div className="App">
-      <h1>你好，世界</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/views/home/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from 'antd';
+import style from './index.module.scss';
+
+export
+function Home() {
+  return <div className={style.com}>
+    <Button>你好，世界</Button>
+  </div>;
+}
