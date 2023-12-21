8faf01cf5cbecd4a4e4c405428c4e2cffac4ed71
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-21 17:26:48 +0800

    删除不需要的代码

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,19 +1,11 @@
 import { Button, Row } from 'antd';
 import style from './index.module.scss';
-import a from './a.module.less';
-import '@/App.scss';
 
 export
 function Home() {
   return <div className={style.com}>
     <Row>
       <Button type="primary">你好，世界</Button>
-    </Row>
-    <Row>
-      <span className={a.color}>你好，世界</span>
-    </Row>
-    <Row>
-      <span className={style.com}>你好，世界</span>
     </Row>
   </div>;
 }
